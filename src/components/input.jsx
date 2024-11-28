3b9b38fa96462ef0e66b2b24895b01992af358bd
orji Dominion <orjidominion32@gmail.com>
2024-11-28 05:25:14 +0100

    stuff's pasting

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -2,50 +2,55 @@ import React, { useState } from 'react';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
-// Extend dayjs with the relativeTime plugin
 dayjs.extend(relativeTime);
 
-const Input = ({data, setData}) => {
-    const [inpChnge, setInpChnge] = useState('');
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const newComment = {
-            content: inpChnge,
-            id: Date.now(),
-            score: 2,
-            createdAt: timeAgo(Date.now()),
-              username: "juliusomo",
-            replies: [],
-        };
-        console.log({
-            ...data, 
-            comments: [newComment, ...data.comments]
-        });
-        setInpChnge('');
+const Input = ({ data, setData }) => {
+  const [inpChnge, setInpChnge] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newComment = {
+      content: inpChnge,
+      id: Date.now(),
+      score: 2,
+      createdAt: timeAgo(Date.now()),
+      user: { 
+        username: "juliusomo",
+        image: {
+          png: "https://example.com/avatar.png", 
+        },
+      },
+      replies: [],
     };
 
-    function timeAgo(date) {
-        // Using dayjs to get the relative time
-        return dayjs(date).fromNow(); // returns something like "a few seconds ago"
-    }
+    setData({
+      ...data,
+      comments: [newComment, ...data.comments], 
+    });
 
-    return (
+    setInpChnge('');
+  };
+
+  function timeAgo(date) {
+    return dayjs(date).fromNow();
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
         <div>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <textarea 
-                        value={inpChnge} 
-                        onChange={(e) => setInpChnge(e.target.value)} 
-                        placeholder='Add a comment'>
-                    </textarea>
-                </div>
-                <div>
-                    <button>send</button>
-                </div>
-            </form>
+          <textarea
+            value={inpChnge}
+            onChange={(e) => setInpChnge(e.target.value)}
+            placeholder="Add a comment"
+          ></textarea>
         </div>
-    );
+        <div>
+          <button>send</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Input;
